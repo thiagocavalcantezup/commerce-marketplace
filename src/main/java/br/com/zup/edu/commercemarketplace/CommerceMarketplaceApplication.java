9dad3f1390f7e55032dd27c5fd72f4a1e029f5dc
Thiago Cavalcante <thiago.oliveira-cavalcante@mailer.com.br>
2022-07-25 19:05:03 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/edu/commercemarketplace/CommerceMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.commercemarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/edu/commercemarketplace/CommerceMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.commercemarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
