57e314c695bf6e75db96b0ce45c24d5518527da9
Thiago Cavalcante <thiago.oliveira-cavalcante@mailer.com.br>
2022-07-27 15:42:00 -0300

    Adicionar handler para erros de validação

--- src/main/java/br/com/zup/edu/commercemarketplace/exceptions/ExceptionHandlers.java
+++ src/main/java/br/com/zup/edu/commercemarketplace/exceptions/ExceptionHandlers.java
@@ -1,19 +1,40 @@
 package br.com.zup.edu.commercemarketplace.exceptions;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class ExceptionHandlers {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlers.class);
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                          WebRequest webRequest) {
+        HttpStatus status = BAD_REQUEST;
+        ErroPadronizado erroPadronizado = getErroPadronizado(status, webRequest);
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(erroPadronizado::adicionar);
+
+        LOGGER.error("MethodArgumentNotValidException: " + ex.getLocalizedMessage(), ex);
+        return ResponseEntity.status(status).body(erroPadronizado);
+    }
+
     public ResponseEntity<?> handleResponseStatus(ResponseStatusException ex,
-            WebRequest webRequest) {
+                                                  WebRequest webRequest) {
         HttpStatus status = ex.getStatus();
         ErroPadronizado erroPadronizado = getErroPadronizado(status, webRequest);
 
